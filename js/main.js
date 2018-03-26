f18dd31f1148846368ce0d0bfa5df73c6a881c65
Darrell DeCosta <dar.deco+git@gmail.com>
2018-03-25 22:57:48 -0400

    Completed work up to unit 7

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//For Funsies
+console.log(["                                     ,(  `-.","                                 ,': `.   `.","                               ,`     `-.    ","                             ,'  ` :     `.  `.","                           ,~     :  .,   `.  `.","                         ,'  ; :   ,       `.  ;","                       ,'  :'       ;  ; ; _,-'","                     .'  ;    '   ; ;'_,_ ;","                   ,;   ;   ` : ;'_,-'   i'","                 ,` `;       ; ','       :","               .';     ; ' ,-'~","             ,'      ;',-.'","           ,(    ; _,-'~  ;","         ,~.`  _','","       .'; _,-' ~","     ,'_;-''","    ,,~","    i'","    :"].join("\n")),console.log("G I M M E // P I Z Z A");
+//Variables
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//Logging
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
