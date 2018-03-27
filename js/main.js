32253e5de6d9d0a8ac33294e4d98b4df21b43a9f
Darrell DeCosta <dar.deco+git@gmail.com>
2018-03-27 18:11:33 -0400

    completed unit 9

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 //For Funsies
-console.log(["                                     ,(  `-.","                                 ,': `.   `.","                               ,`     `-.    ","                             ,'  ` :     `.  `.","                           ,~     :  .,   `.  `.","                         ,'  ; :   ,       `.  ;","                       ,'  :'       ;  ; ; _,-'","                     .'  ;    '   ; ;'_,_ ;","                   ,;   ;   ` : ;'_,-'   i'","                 ,` `;       ; ','       :","               .';     ; ' ,-'~","             ,'      ;',-.'","           ,(    ; _,-'~  ;","         ,~.`  _','","       .'; _,-' ~","     ,'_;-''","    ,,~","    i'","    :"].join("\n")),console.log("G I M M E // P I Z Z A");
+console.log(["                                     ,(  `-.", "                                 ,': `.   `.", "                               ,`     `-.    ", "                             ,'  ` :     `.  `.", "                           ,~     :  .,   `.  `.", "                         ,'  ; :   ,       `.  ;", "                       ,'  :'       ;  ; ; _,-'", "                     .'  ;    '   ; ;'_,_ ;", "                   ,;   ;   ` : ;'_,-'   i'", "                 ,` `;       ; ','       :", "               .';     ; ' ,-'~", "             ,'      ;',-.'", "           ,(    ; _,-'~  ;", "         ,~.`  _','", "       .'; _,-' ~", "     ,'_;-''", "    ,,~", "    i'", "    :"].join("\n")), console.log("G I M M E // P I Z Z A");
 //Variables
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-//Logging
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var checkForMatch = function() {
+    if (cardsInPlay.length >= 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+        } else {
+            console.log("Sorry, try again.");
+        }
+    } else {
+    	console.log ("You need to flip another card")
+    }
+}
+
+var flipCard = function(cardId) {
+    console.log("User filpped " + cards[cardId])
+    cardsInPlay.push(cards[cardId])
+    checkForMatch()
+}
+
+flipCard(2)
+flipCard(3)
